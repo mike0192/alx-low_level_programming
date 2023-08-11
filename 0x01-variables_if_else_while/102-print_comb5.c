2054f41d1ee8b6bfa9ade1841ced69399e92cef8
michael <michaelhagos163@gmail.com>
2023-08-11 15:14:41 -0700

    j

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,27 +6,27 @@
  */
 int main(void)
 {
-int i, j;
+    int i, j;
 
-for (i = 0; i <= 99; i++)
-{
-for (j = i; j <= 99; j++)
-{
-putchar((i / 10 + '0');
-putchar((i % 10 + '0');
-putchar((' ');
-putchar((j / 10 + '0');
-putchar((j % 10 + '0');
+    for (i = 0; i <= 99; i++)
+    {
+        for (j = i; j <= 99; j++)
+        {
+            putchar(i / 10 + '0');
+            putchar(i % 10 + '0');
+            putchar(' ');
+            putchar(j / 10 + '0');
+            putchar(j % 10 + '0');
 
-if (i != 99 || j != 99)
-{
-putchar(',');
-putchar(' ');
-}
-}
-}
-}
-putchar('\n');
+            if (i != 98 || j != 99)
+            {
+                putchar(',');
+                putchar(' ');
+            }
+        }
+    }
 
-return (0);
+    putchar('\n');
+
+    return (0);
 }
