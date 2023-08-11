b2cd11d54e55603daa1bfadabdd7d92d08f23af8
michael <michaelhagos163@gmail.com>
2023-08-11 14:30:30 -0700

    m

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -6,17 +6,11 @@
  */
 int main(void)
 {
-putchar('0' + 0);
-putchar('0' + 1);
-putchar('0' + 2);
-putchar('0' + 3);
-putchar('0' + 4);
-putchar('0' + 5);
-putchar('0' + 6);
-putchar('0' + 7);
-putchar('0' + 8);
-putchar('0' + 9);
-
+int i;
+for (i = 48; i < 58; i++)
+{
+putchar(i);
+}
 putchar('\n');
 
 return (0);
