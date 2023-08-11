c3249b1ae2d81bd1c8ab81ae82b34bbabd62efc4
michael <michaelhagos163@gmail.com>
2023-08-11 15:02:30 -0700

    j

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -18,7 +18,8 @@ putchar(' ');
 putchar(j / 10 + '0');
 putchar(j % 10 + '0');
 
-if (i != 98 || j != 99)
+if (i != 99 || j != 99)
+{
 putchar(',');
 putchar(' ');
 }
@@ -28,6 +29,4 @@ putchar(' ');
 putchar('\n');
 
 return (0);
-
-}
 }
