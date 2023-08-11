9bae9f1192ada02d27c7c3f970062e705c85e2e6
michael <michaelhagos163@gmail.com>
2023-08-11 15:10:21 -0700

    j

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,11 +12,11 @@ for (i = 0; i <= 99; i++)
 {
 for (j = i; j <= 99; j++)
 {
-putchar(i / 10 + '0');
-putchar(i % 10 + '0');
-putchar(' ');
-putchar(j / 10 + '0');
-putchar(j % 10 + '0');
+putchar((i / 10 + '0');
+putchar((i % 10 + '0');
+putchar((' ');
+putchar((j / 10 + '0');
+putchar((j % 10 + '0');
 
 if (i != 99 || j != 99)
 {
