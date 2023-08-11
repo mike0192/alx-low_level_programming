d417e7c34d3f2c03ae8b4f6c24bc4b6d400a00bf
michael <michaelhagos163@gmail.com>
2023-08-11 14:18:46 -0700

    m

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -10,16 +10,100 @@ int main(void)
 {
 int n;
 
-srand(time(0));
+srand(time(0)); 
 n = rand() - RAND_MAX / 2;
 
-printf("Last digit of %d is ", n % 10);
-if ((n % 10) > 5)
-printf("and is greater than 5\n");
-else if ((n % 10) == 0)
-printf("and is 0\n");
+int lastDigit = n % 10;
+
+putchar('L');
+putchar('a');
+putchar('s');
+putchar('t');
+putchar(' ');
+putchar('d');
+putchar('i');
+putchar('g');
+putchar('i');
+putchar('t');
+putchar(' ');
+putchar('o');
+putchar('f');
+putchar(' ');
+putchar(n + '0');
+putchar(' ');
+putchar('i');
+putchar('s');
+putchar(' ');
+
+if (lastDigit > 5)
+{
+putchar('a');
+putchar('n');
+putchar('d');
+putchar(' ');
+putchar('i');
+putchar('s');
+putchar(' ');
+putchar('g');
+putchar('r');
+putchar('e');
+putchar('a');
+putchar('t');
+putchar('e');
+putchar('r');
+putchar(' ');
+putchar('t');
+putchar('h');
+putchar('a');
+putchar('n');
+putchar(' ');
+putchar('5');
+putchar('\n');
+}
+else if (lastDigit == 0)
+{
+putchar('a');
+putchar('n');
+putchar('d');
+putchar(' ');
+putchar('i');
+putchar('s');
+putchar(' ');
+putchar('0');
+putchar('\n');
+}
 else
-printf("and is less than 6 and not 0\n");
+{
+putchar('a');
+putchar('n');
+putchar('d');
+putchar(' '); 
+putchar('i');
+putchar('s');
+putchar(' ');
+putchar('l');
+putchar('e');
+putchar('s');
+putchar('s');
+putchar(' ');
+putchar('t');
+putchar('h');
+putchar('a');
+putchar('n');
+putchar(' ');
+putchar('6');
+putchar(' ');
+putchar('a');
+putchar('n');
+putchar('d');
+putchar(' ');
+putchar('n');
+putchar('o');
+putchar('t');
+putchar(' ');
+putchar('0');
+putchar('\n');
+}
 
 return (0);
 }
