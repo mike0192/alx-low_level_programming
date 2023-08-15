47922a3728d85dbd501e3383bdfc68ae952bd9e4
michael <michaelhagos163@gmail.com>
2023-08-15 11:50:50 -0700

    k

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * Description: prints the alphabet, in lowercase
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+int i;
+
+for (i = 97; i <= 122; i++)
+{
+_putchar(i);
+}
+}
