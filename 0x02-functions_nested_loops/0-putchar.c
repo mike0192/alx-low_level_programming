37925dd34b1da89229c9986c79b7570d38150dc3
michael <michaelhagos163@gmail.com>
2023-08-15 10:48:48 -0700

    alawkm lmokr

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - entry point
+ * Description: 'prints _putchar to stdout'
+ * Return 0
+ */
+
+int main(void)
+{
+int _putchar;
+{
+putchar("_putchar.\n")
+return 0;
+}
+}
