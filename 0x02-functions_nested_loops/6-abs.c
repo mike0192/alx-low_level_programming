5aff5e8382d7c69a5e2d1ab5c8de51ac9d2cf9d4
michael <michaelhagos163@gmail.com>
2023-08-15 13:48:38 -0700

    sd

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _abs - show i if the input is a
+ * letter another cases, shows 0
+ * @c: the character in ASCII code
+ *
+ * Return: 1 for letters, 0 otherwise
+ */
+int _abs(int i)
+{
+if (i >= 0)
+{
+return (i);
+}
+else 
+{
+return (i * -1);
+}
+}
