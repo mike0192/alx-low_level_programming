09043b5cf5ae1762a35368a6a5cfbde818c14880
michael <michaelhagos163@gmail.com>
2023-08-15 12:21:19 -0700

    ho

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+int i=10;
+
+while(i<5)
+{
+_putchar("i = 97; i <= 122");
+i++;
+}
+_putchar('\n');
+}
