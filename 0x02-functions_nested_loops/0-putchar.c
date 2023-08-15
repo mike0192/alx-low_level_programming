444b67864194f273b9bc41dae1538dae3f185ef4
michael <michaelhagos163@gmail.com>
2023-08-15 11:18:36 -0700

    m

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,20 @@
 #include "main.h"
+
 /**
  * main - entry point
- * Description: 'prints _putchar to stdout'
- * Return 0
+ * Description: prints _putchar to stdout
+ * Return: 0
  */
-
 int main(void)
 {
-_putchar("_");
-_putchar("p");
-_putchar("u");
-_putchar("t");
-_putchar("c");
-_putchar("h");
-_putchar("a");
-_putchar("r");
-_putchar("\n");
-return 0;
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
