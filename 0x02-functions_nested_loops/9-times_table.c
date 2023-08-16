055e40636cf379ea055cd7e3c22848693f237b58
michael <michaelhagos163@gmail.com>
2023-08-16 07:29:59 -0700

    ds

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,32 +6,28 @@
  */
 void times_table(void)
 {
-int i, j, k;
+int i, j, k, l ;
 
-for (i = 0; i <= 10; i++)
+for (i = 0; i <= 2; i++)
 {
-for (j = 0; j <= 10; j++)
+for (j = 0; j <= 9; j++)
 {
-
-k = i * j;
-if (j == 0)
+for ((i <= 1 j <= 9) || (i <= 2 && j <= 3))
 {
+for (k = 0; k <= 5; k++)
+{
+for (l = 0; l <= 9; l++)
+{
+_putchar(i + '0');
+_putchar(j + '0');
+_putchar(58);
 _putchar(k + '0');
-}
-if (k < 10 && j != 0)
-{
-_putchar(',');
-_putchar(' ');
-_putchar(' ');
-_putchar(k + '0');
-}
-else if (k >= 10)
-{
-_putchar(',');
-_putchar(' ');
-_putchar((k / 10) + '0');
-_putchar((k % 10) + '0');
-}
-}
+_putchar(l + '0');
 _putchar('\n');
+}
+}
+}
+}
+}
+}
 }
