8cd516dec92477395894cd73db53715dd343e29f
michael <michaelhagos163@gmail.com>
2023-08-16 01:33:26 -0700

    yu

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,7 +2,8 @@
 
 /**
  * _abs - computes the absolute value of an integer.
- * @c: the int to be checked
+ * @n: the int to be checked
+ *
  * Return: the absolute value of int
  */
 int _abs(int n)
