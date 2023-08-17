e35251b13ff8646d74093fc91bc2b318df61da58
michael <michaelhagos163@gmail.com>
2023-08-17 10:52:43 -0700

    :k;

---
+++ 0x04-more_functions_nested_loops/0-issupper.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int _isupper(int c)
+{
+int c;
+{
+if (c >= 65 && c <= 90);
+return (1);
+}
+else
+{
+return (0);
+}
+}
