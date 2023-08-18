3295079aec6881507b57a2625ff6d2d323aa261f
michael <michaelhagos163@gmail.com>
2023-08-18 03:46:57 -0700

    numbers have life

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_number - Prints an integer
+ * print_number - func prints an integer
  * @n: The integer to be printed
  */
 void print_number(int n)
@@ -10,12 +10,12 @@ unsigned int num = n;
 /*first check if its negative*/
 if (n < 0)
 {
-putchar('-');
+_putchar('-');
 num = -num;
 }
 /*print the first few digits*/
 if ((num / 10) > 0)
 print_number(num / 10);
 /*print the last digit*/
-putchar((num % 10) + '0');
+_putchar((num % 10) + '0');
 }
