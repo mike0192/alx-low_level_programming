5e45d89870124c981df18640def5425a3f0b4693
michael <michaelhagos163@gmail.com>
2023-09-11 09:41:13 -0700

    SOFT

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -10,9 +10,9 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner) 
 {
-strncpy(d->name, name, sizeof(d->name) - 1);
-d->name[sizeof(d->name) - 1] = '\0';
+if (d == NULL)
+d = malloc(sizeof(struct dog));
+d->name = name;
 d->age = age;
-strncpy(d->owner, owner, sizeof(d->owner) - 1);
-d->owner[sizeof(d->owner) - 1] = '\0';
+d->owner = owner;
 }
