6099f2cf026d53694834ac8be4b1375dc0bba2ad
michael <michaelhagos163@gmail.com>
2023-09-14 11:02:51 -0700

    last chance

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -16,16 +16,17 @@ va_list args;
 char *str;
 unsigned int i;
 va_start(args, n);
+
 for (i = 0; i < n; i++)
 {
 str = va_arg(args, char *);
-}
 if (str == NULL)
 printf("(nil)");
 else
 printf("%s", str);
 if (separator != NULL && i != (n - 1))
 printf("%s", separator);
+}
 printf("\n");
 va_end(args);
 }
