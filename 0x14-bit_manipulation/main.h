fbedf6c23169a9ae30f7dd4caa71130d7a00088e
michael <michaelhagos163@gmail.com>
2023-09-29 09:17:40 -0700

    boom

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
+int _atoi(const char *s);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
