f7c2a91b9374b9767bd91adcdb108f879b00d4f6
michael <michaelhagos163@gmail.com>
2023-10-02 09:37:57 -0700

    se

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,6 +5,13 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <elf.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
