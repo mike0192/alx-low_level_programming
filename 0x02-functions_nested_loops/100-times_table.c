ef5f0fa9dbd9e39e4ca78b89ba4452e18f09f716
michael <michaelhagos163@gmail.com>
2023-11-13 11:32:10 -0800

    sonder

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,37 +1,46 @@
-#include <stdio.h>
-/*
- * print_times_table - prints time table of n
- *
- * @n: takes number input
- */
+#include "main.h"
+
+
+/**
+  * print_times_table - printsthe times inputed
+  * starting with 0.
+  *@n: the value of the timea table printed
+  */
+
+
 void print_times_table(int n)
+{
 
-int pdx, mtx, nux;
 
-if (n <= 15 && n >= 0)
-{
-for (nux = 0; nux <= n; nux++)
-{
-_putchar(48);
-for (mtx = 1; mtx <= n; mtx++)
-{
-_putchar(',');
-_putchar(' ');
-pdx = nux * mtx;
-if (pdx <= 9)
-_putchar(' ');
-if (pdx <= 99)
-_putchar(' ');
-if (pdx >= 100)
-{
-_putchar((pdx / 100) + 48);
-_putchar((pdx / 10) % 10 + 48);
-}
-else if
-_putchar((pdx <= 99) && pdx >= 10)
-_putchar((pdx / 10) + 48);
-_putchar((pdx % 10) + 48);
-}
-_putchar('\n');
-}
+	int num, mult, prod;
+
+
+	if (n >= 0 && n <= 15)
+	{
+	for (num = 0; num <= n; num++)
+	{
+	_putchar('0');
+	for (mult = 1; mult <= n; mult++)
+	{
+	_putchar(',');
+	_putchar(' ');
+	prod = num * mult;
+	if (prod <= 99)
+	_putchar(' ');
+	if (prod <= 9)
+	_putchar(' ');
+	if (prod >= 100)
+	{
+	_putchar((prod / 100) + '0');
+	_putchar(((prod / 10)) % 10 + '0');
+	}
+	else if (prod <= 99 && prod >= 10)
+	{
+	_putchar((prod / 10) + '0');
+	}
+	_putchar((prod % 10) + '0');
+	}
+	_putchar('\n');
+	}
+	}
 }
--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,25 +1,21 @@
 #include "main.h"
-#include <stdio.h>
+
+
 /**
- * main - Entry point
+ * main - check the code.
  *
- * Description: computes the sum of all 
- *              the multiples of 3 or 5
- *              below 1024 (excluded)
- * Return: Always 0
+ * Return: Always 0.
  */
 
-int main(void) {
-int limit = 1024;
-int sum = 0;
 
-for (int i = 1; i < limit; i++) {
-if (i % 3 == 0 || i % 5 == 0) {
-sum += i;
-}
-}
-
-printf("The sum of multiples of 3 or 5 below %d is: %d\n", limit, sum);
-
-return (0);
+int main(void)
+{
+	print_times_table(3);
+	_putchar('\n');
+	print_times_table(5);
+	_putchar('\n');
+	print_times_table(98);
+	_putchar('\n');
+	print_times_table(12);
+	return (0);
 }
